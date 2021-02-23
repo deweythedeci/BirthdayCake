4d274bbe49122fb832020c393e7f6f19515b4b34
dewey <a874589@gmail.com>
2021-02-22 20:44:59 -0800

    birthday cake

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -2,6 +2,8 @@ package cs301.birthdaycake;
 
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.util.Log;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goodbye(View button){
+        Log.i("button", "Goodbye");
     }
 }
